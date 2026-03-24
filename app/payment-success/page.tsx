febb43e3a478daa35c0795208556e4a00db2d080
Ingyin-Phyo275 <xiluhan.igp@gmail.com>
2026-03-24 11:05:34 +0630

    Remove console.log

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { getUsers } from "../../server/actions/testActions";
+// import { getUsers } from "../../server/actions/testActions";
 
 export default async function page() {
   
-  const data = await getUsers();
-  console.log("User data in payment", data?.data);
+  // const data = await getUsers();
+  // console.log("User data in payment", data?.data);
 
   return (
     <div className="flex flex-1 flex-col w-full items-center justify-center my-auto min-h-screen">
