82aed59c137b7477e2fe038e8ce838b12a284ec0
Ingyin-Phyo275 <xiluhan.igp@gmail.com>
2026-03-25 13:35:02 +0630

    Add secret key

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,5 +1,7 @@
 
 export default function page() {
+    console.log("secret", process.env.SECRET_KEY)
+
   return (
     <div className='flex flex-1 w-full h-screen items-center justify-center text-primary mx-auto'>Payment Success Page</div>
   )
--- lib/crypto.ts
+++ lib/crypto.ts
@@ -1,20 +1,12 @@
 // /lib/crypto.ts
 import crypto from "crypto";
 
-const SECRET_KEY = Buffer.from("12345678901234567890123456789012");
-
-export function decryptData(
-  encryptedBase64: string,
-  ivBase64: string
-): string {
+const SECRET_KEY = Buffer.from(process.env.SECRET_KEY || "", "utf8");
+export function decryptData(encryptedBase64: string, ivBase64: string): string {
   const encryptedBuffer = Buffer.from(encryptedBase64, "base64");
   const iv = Buffer.from(ivBase64, "base64");
 
-  const decipher = crypto.createDecipheriv(
-    "aes-256-gcm",
-    SECRET_KEY,
-    iv
-  );
+  const decipher = crypto.createDecipheriv("aes-256-gcm", SECRET_KEY, iv);
 
   // AES-GCM: last 16 bytes = authTag
   const authTag = encryptedBuffer.slice(-16);
